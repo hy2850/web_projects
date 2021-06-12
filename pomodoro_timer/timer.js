b179647094f4bd60d3bd8727fa192639f01c70a2
hy2850 <39735858+hy2850@users.noreply.github.com>
2021-06-13 01:57:20 +0900

    Asynced 'start', 'timer' btw two timers
    - Stored paired timer variables in the arrays (SHORT = 0, LONG = 1)
    - Added separate listeners to 'start' and 'reset' button
    - Refactored to fit new array variables
    
    Limited to two timers (further work : react componentize to create more than two timers)

--- pomodoro_timer/timer.js
+++ pomodoro_timer/timer.js
@@ -1,42 +1,60 @@
+// Variables
 const MINIUTE = 60;
+const SHORT = 0, LONG = 1;
+let init_time = [Math.floor(60*MINIUTE), Math.floor(60*MINIUTE)]
 
-// FOR TESTING
-// let init_time = Math.floor(0.1*MINIUTE) - 4;
-///
+// Elements and Event listeners 
+const short_box = document.querySelector('.timerBox[data-short]');
+const long_box = document.querySelector('.timerBox[data-long]');
 
-let init_time = Math.floor(60*MINIUTE) // in seconds
+const short_start = short_box.querySelector('.button.start');
+const long_start = long_box.querySelector('.button.start');
+short_start.onclick = ()=>{countDown(SHORT)};
+long_start.onclick = ()=>{countDown(LONG)};
 
-let update_time = (time)=>{
+const short_reset = short_box.querySelector('.button.reset');
+const long_reset = long_box.querySelector('.button.reset');
+short_reset.onclick = ()=>{reset(SHORT)};
+long_reset.onclick = ()=>{reset(LONG)};
+
+//======================================================
+// Main functions
+
+timer = [short_box.querySelector('#timer'), long_box.querySelector('#timer')];
+let update_time = (time, clockIdx = SHORT)=>{ 
     var min = Math.floor(time/MINIUTE); min = min.toString();
     var sec = time%MINIUTE; sec = sec.toString();
-    document.getElementById("timer").innerHTML = min.padStart(2, '0') + ":" + sec.padStart(2, '0');
+    timer[clockIdx].innerHTML = min.padStart(2, '0') + ":" + sec.padStart(2, '0');
 }        
-update_time(init_time); // init
+// init clock
+update_time(init_time[SHORT], SHORT);
+update_time(init_time[LONG], LONG);
 
-let started = false; // only respond to 'start' when started == false
-let refreshIntervalId = null; // setInterval ID for clearing
-function countDown(){
-    if (started) return;
-    else started = true;
 
-    let time = init_time;
-    refreshIntervalId = setInterval(() => {
+let didStart = [false, false]; // only respond to 'start' when started == false
+let refreshInterval = [null, null] // setInterval ID for clearing
+function countDown(clockIdx = SHORT){
+    if (didStart[clockIdx]) return;
+    else didStart[clockIdx] = true;
+
+    time = init_time[clockIdx];
+    refreshInterval[clockIdx] = setInterval(() => {
         if(time == 0) {
             beep();
-            reset();
+            reset(clockIdx);
             return;
         }
         time--;
-        update_time(time);        
+        update_time(time, clockIdx);        
     }, 1000);
-    
-    //alert("End!");
 } 
 
-function reset(){
-    clearInterval(refreshIntervalId);
-    started = false;
-    update_time(init_time);
+function reset(clockIdx = SHORT){
+    clearInterval(refreshInterval[clockIdx]);
+    refreshInterval[clockIdx] = null;
+
+    didStart[clockIdx] = false;
+    update_time(init_time[clockIdx], clockIdx);
 } 
 
 function beep() {
